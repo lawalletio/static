70d949309f11789a873df0f495bf8799bc4ecf66
Agustin Kassis <webmaster@masize.com>
2024-03-29 16:58:51 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# LaWallet Static
+
+This repo contains static files accesed via https://static.lawallet.ar
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import "./style.css";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <h1>La Wallet Static</h1>
+  </div>
+`;
